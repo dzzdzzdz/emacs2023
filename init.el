e790a9bf2d584924d6e8795af84085dd64e9cb3a
dzzdzzdz <danielzhaozd@gmail.com>
2023-01-11 16:47:45 +0100

    helpful

--- init.el
+++ init.el
@@ -94,3 +94,14 @@
   :diminish which-key-mode
   :config
   (setq which-key-idle-delay 1))
+
+;; helpful
+(use-package helpful
+  :custom
+  (counsel-describe-function-function #'helpful-callable)
+  (counsel-describe-variable-function #'helpful-variable)
+  :bind
+  ([remap describe-function] . counsel-describe-function)
+  ([remap describe-command] . helpful-command)
+  ([remap describe-variable] . counsel-describe-variable)
+  ([remap describe-key] . helpful-key))
