cf3fda1efe082e7d78563e239878ed82cb687270
dzzdzzdz <danielzhaozd@gmail.com>
2023-01-11 16:41:28 +0100

    counsel

--- init.el
+++ init.el
@@ -42,6 +42,14 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
+;; counsel
+(use-package counsel
+  :bind (("M-x" . counsel-M-x)
+         ("C-x b" . counsel-ibuffer)
+         ("C-x C-f" . counsel-find-file)
+         :map minibuffer-local-map
+         ("C-r" . 'counsel-minibuffer-history)))
+
 ;; swiper
 (use-package swiper)
 
